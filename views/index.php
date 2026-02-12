adb304c8bc3c8658ff24c1f1ccab751edc3c75ac
santimnv <santimnv27@gmail.com>
2026-02-12 19:32:25 +0100

    Refactor user interface in index.php and layout.php
    - Updated the header to display "Users" and enhanced the user list presentation with Bootstrap styling.
    - Improved the form for creating new users with a responsive layout and validation.
    - Added a navigation bar to layout.php for better application structure and user experience.
    - Included viewport meta tag for responsive design.

--- views/index.php
+++ views/index.php
@@ -1,34 +1,65 @@
-<h1>PHP Test Application</h1>
+<div class="page-header">
+  <h1>Users</h1>
+</div>
 
-<table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>E-mail</th>
-			<th>City</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?foreach($users as $user){?>
-		<tr>
-			<td><?=$user->getName()?></td>
-			<td><?=$user->getEmail()?></td>
-			<td><?=$user->getCity()?></td>
-		</tr>
-		<?}?>
-	</tbody>
-</table>				
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">User List</h3>
+  </div>
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>E-mail</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($users as $user) { ?>
+        <tr>
+          <td><?= $user->getName() ?></td>
+          <td><?= $user->getEmail() ?></td>
+          <td><?= $user->getCity() ?></td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+</div>
 
-<form method="post" action="create.php">
-	
-	<label for="name">Name:</label>
-	<input name="name" input="text" id="name"/>
-	
-	<label for="email">E-mail:</label>
-	<input name="email" input="text" id="email"/>
-	
-	<label for="city">City:</label>
-	<input name="city" input="text" id="city"/>
-	
-	<button>Create new row</button>
-</form>
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Create New User</h3>
+  </div>
+  <div class="panel-body">
+    <form method="post" action="create.php" class="form-horizontal">
+      <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Name</label>
+        <div class="col-sm-10">
+          <input type="text" name="name" id="name" class="form-control" required>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="email" class="col-sm-2 control-label">E-mail</label>
+        <div class="col-sm-10">
+          <input type="email" name="email" id="email" class="form-control" required>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="city" class="col-sm-2 control-label">City</label>
+        <div class="col-sm-10">
+          <input type="text" name="city" id="city" class="form-control" required>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-primary">Create new row</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
--- views/layout.php
+++ views/layout.php
@@ -2,26 +2,33 @@
 <html>
   <head>
   	<meta charset="UTF-8">
-  	
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>PHP Test Application</title>
-    
+
 	<link href="favicon.ico" type="image/x-icon" rel="icon" />
-	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />	
-    
+	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />
+
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/application.css">
-    
+
     <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
 	<script type="text/javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
 
   </head>
   <body>
 
-  <div class="container">
-    
-    <?= $content ?>
-    
-  </div>  
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <span class="navbar-brand">PHP Test Application</span>
+        </div>
+      </div>
+    </nav>
+
+    <main class="container">
+      <?= $content ?>
+    </main>
 
   </body>
 </html>
