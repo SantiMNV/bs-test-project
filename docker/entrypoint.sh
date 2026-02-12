9d5b3f753fd41ca17aedda69c6edee1efb3a9a1c
santimnv <santimnv27@gmail.com>
2026-02-12 22:31:04 +0100

    deploy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,10 +7,10 @@ cat > /var/www/html/config/database.php <<PHP
 <?php
 
 \$database = array(
-    'address'  => '${DB_HOST:-db}',
-    'username' => '${DB_USER:-app}',
-    'password' => '${DB_PASSWORD:-app}',
-    'database' => '${DB_NAME:-php_test}'
+    'address'  => '${MYSQL_HOST}',
+    'username' => '${MYSQL_USER}',
+    'password' => '${MYSQL_PASSWORD}',
+    'database' => '${MYSQL_DATABASE}'
 );
 PHP
 
